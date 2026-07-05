0275263953b55c8cb0179d154af26f26165e54e0
rajabovdavlat <rajabovoff1@gmail.com>
2026-07-05 12:13:17 +0500

    feat: add http requests with vue-resource

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <add-blog></add-blog>
+  </div>
+</template>
+
+<script>
+import addBlog from './components/addBlog.vue'
+
+export default {
+  components: {
+    'add-blog': addBlog
+  },
+ 
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
