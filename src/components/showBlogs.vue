2dfa710187104b1834a70a46c7cb596ab22ebfa7
rajabovdavlat <rajabovoff1@gmail.com>
2026-07-06 15:45:11 +0500

    Retrieving Posts from Firebase

--- src/components/showBlogs.vue
+++ src/components/showBlogs.vue
@@ -3,14 +3,16 @@
     <h1>All Blog Articles</h1>
     <input type="text" v-model="search" placeholder="search blog" />
     <div v-for="blog in filteredBlogs" class="single-blog">
-      <router-link :to="'/blog/' + blog.id"><h2>{{ blog.title | to-uppercase }}</h2></router-link>
-      <article>{{ blog.body | snippet }}</article>
+      <router-link :to="'/blog/' + blog.id"
+        ><h2>{{ blog.title | (to - uppercase) }}</h2></router-link
+      >
+      <article>{{ blog.content | snippet }}</article>
     </div>
   </div>
 </template>
 
 <script>
-import searchMixin from '../mixins/searchMixin'
+import searchMixin from "../mixins/searchMixin";
 
 export default {
   data() {
@@ -21,21 +23,27 @@ export default {
   },
   created() {
     this.$http
-      .get("https://jsonplaceholder.typicode.com/posts")
+      .get(
+        "https://vue-project-blog-d102c-default-rtdb.firebaseio.com/posts.json"
+      )
       .then(function (data) {
-        console.log(data);
-        this.blogs = data.body.slice(0, 10);
-      });
+        return data.json();
+      }).then(function (data) {
+        var blogsArray = [];
+        for (let key in data) {
+          data[key].id = key
+          blogsArray.push(data[key])
+        }
+        this.blogs = blogsArray
+      })
   },
-  computed: {
-  
-  },
+  computed: {},
   filters: {
     "to-uppercase": function (value) {
       return value.toUpperCase();
     },
   },
-  mixins:[searchMixin]
+  mixins: [searchMixin],
 };
 </script>
 
